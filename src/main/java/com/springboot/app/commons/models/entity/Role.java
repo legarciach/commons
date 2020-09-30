370b74e25e06327b67776fa2ef6921275305c600
Enrique Garcia <enrique.escom@gmail.com>
2020-09-29 23:35:50 -0500

    Nuevas clases

---
+++ src/main/java/com/springboot/app/commons/models/entity/Role.java
@@ -0,0 +1,38 @@
+package com.springboot.app.commons.models.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "roles")
+public class Role implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, length = 30)
+    private String nombre;
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    private static final long serialVersionUID = -5479176319082338162L;
+}
